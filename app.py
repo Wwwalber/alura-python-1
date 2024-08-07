58ada444ae1cc2875d9fc000a00ffe3749482b27
Walber <walberoc@gmail.com>
2024-08-07 13:25:34 -0300

    menu com if's

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
 ┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
@@ -14,7 +16,20 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção:')
-#print("Você escolheu a opção",opcao_escolhida)
-print(f'Você escolheu a opção {opcao_escolhida}\n')
-import this
+opcao_escolhida = int(input('Escolha uma opção:'))
+#opcao_escolhida = int(opcao_escolhida)
+
+def finalizar_app():
+    os.system('clear')
+    print('Saindo da Aplicação...\n')
+    exit()
+
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurantes')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+    finalizar_app()

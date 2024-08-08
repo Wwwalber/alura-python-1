4b28a8d0cf24a1d29530c59a695e5b1eac7c7f3c
walber <walberoc@gmail.com>
2024-08-08 14:28:59 -0300

    organizando principal em chamadas de funções

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-print("""
+def exibir_nome_do_programa():
+    print("""
 ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
 ┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
 ┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
@@ -10,26 +11,39 @@ print("""
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
 """)
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção:'))
+    #opcao_escolhida = int(opcao_escolhida)
 
-opcao_escolhida = int(input('Escolha uma opção:'))
-#opcao_escolhida = int(opcao_escolhida)
+    match opcao_escolhida:
+        case 1:
+            print('Cadastrar restaurantes')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurantes')
+        case 4:
+            finalizar_app()
+        case _:
+            print('Opção inválida!')
+            finalizar_app()
 
 def finalizar_app():
     os.system('clear')
     print('Saindo da Aplicação...\n')
     exit()
 
+def main():
+    exibir_nome_do_programa() # função que controla o projeto
+    exibir_opcoes()
+    escolher_opcoes()
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurantes')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurantes')
-else:
-    finalizar_app()
+# .py posso informa que é o arquivo principal do programa
+if __name__ == '__main__':
+    main()

874ce7f3b609b7d3ab3c657eea817ca3d56dcc37
Walber <walberoc@gmail.com>
2024-08-28 12:38:40 -0300

    usando decorator 'Property' para status

--- python-curso2-oo/modelos/restaurante.py
+++ python-curso2-oo/modelos/restaurante.py
@@ -4,16 +4,20 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome # self self que está vindo. Referencia do self que está chamendo
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False # _ infoma que o atributo está protegidom, embora não privado
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
 
     def listar_restaurantes():
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes: # somente restaurates
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante._ativo}')
 
+    @property
+    def ativo(self):
+        return 'Ativo ❎' if self._ativo else 'Inativo ❎'
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 

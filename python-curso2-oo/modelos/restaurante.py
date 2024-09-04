c0998d9ff3eef6418fa4a2b157dad62a43297cd5
Walber <walberoc@gmail.com>
2024-09-04 11:02:04 -0300

    classe Livro inicial

--- python-curso2-oo/modelos/restaurante.py
+++ python-curso2-oo/modelos/restaurante.py
@@ -2,25 +2,31 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome # self self que está vindo. Referencia do self que está chamendo
-        self.categoria = categoria
+        self._nome = nome.title() # self self que está vindo. Referencia do self que está chamendo
+        self.categoria = categoria.upper()
         self._ativo = False # _ infoma que o atributo está protegidom, embora não privado
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
 
-    def listar_restaurantes():
+    @classmethod    #indicar que é um método da classe
+    def listar_restaurantes(cls):  # cls é a convenção para indicar que uaremos informaçõe referentes a esse método 
+                                    # agora, em vez de 'Restaurante' usar 'cls'
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes: # somente restaurates
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante._ativo}')
+        for restaurante in cls.restaurantes: # somente restaurates
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante._ativo}')
 
     @property
     def ativo(self):
         return 'Ativo ❎' if self._ativo else 'Inativo ❎'
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
+    # método para classe instanciada
+    def alternar_estado(self): # informar a referencia > self
+        self._ativo = not self._ativo
 
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza', 'italiana')
 
 Restaurante.listar_restaurantes()

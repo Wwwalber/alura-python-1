b480e54dd1080790e9e765bbfef235878f187ffa
walber <walberoc@gmail.com>
2024-09-13 08:05:47 -0300

    usar superclasse

--- python-curso2-oo/modelos/cardapio/prato.py
+++ python-curso2-oo/modelos/cardapio/prato.py
@@ -2,4 +2,5 @@ from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Prato(ItemCardapio):  # informa que vai herdar métodos, atributos de uma outra classe
     def __init__(self, nome, preco, descricao):
-        
+        super().__init__(nome, preco)  # vai na classe principal por meio do 'siper'
+        self.descricao # a descrição 
